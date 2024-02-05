50225bd9583178b880b6ef098ebc544a42e4fd85
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-02-05 17:33:10 +0500

    commit

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,17 @@
+import conf from '../conf.js'
+import { Client, Account, ID } from "appwrite";
+
+
+
+
+export class AuthService{
+client = new Client();
+account;
+
+
+}
+
+
+
+const authService = new AuthService();
+export default authService;
