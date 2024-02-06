a64f460dd28cdc779722c7e1bab571fb85afdcfb
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-02-06 22:01:45 +0500

    commit

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from '../conf.js'
+import conf from '../conf/conf.js'
 import { Client, Account, ID } from "appwrite";
 
 
@@ -9,9 +9,65 @@ client = new Client();
 account;
 
 
+constructor(){
+    this.client
+    .setEndpoint(conf.appwriteUrl) // Your API Endpoint
+    .setProject(conf.appwriteProjectId) // Your project ID
+    this.account = new Account(this.client);
+}
+async createAccount({email, password, name}){ 
+try{
+const userAccount=await this.account.create(ID.unique(),
+email, password, name);
+if(userAccount){
+
+    return this.login({email, password})
+
+}
+    else{
+        return userAccount
+    }
+
+}catch(e){
+    console.error(e);
+
+
+}
+}
+
+async login({email, password}){
+    try {
+    return    await this.account.createEmailSession(email, password);
+
+        
+    } catch (error) {
+        throw error
+        
+    }
+}
+async getCurrentUser(){
+    try {
+      return await this.account.get();
+        
+    } catch (error) {
+     throw error;   
+    }
+    return null
+
+}
+async logout(){
+    try {
+        return await this.account.deleteSessions();
+        
+    } catch (error) {
+        throw error;
+}
+
+}
 }
 
 
 
 const authService = new AuthService();
+
 export default authService;
