cb8de862e85c93216ba4ff3018e94bf4dd6df39d
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-02-07 23:42:01 +0500

    commit

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -2,14 +2,50 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import authService from './appwrite/auth'
+import {login,logout} from "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
+const [ loading,setLoading]= useState(true)
+const dispatch = useDispatch()
 
-  return (
-    <>
-     FFFFFFFFFFFFF
-    </>
-  )
+
+useEffect(() => {
+  authService.getCurrentUser().then((userData)=>{
+    if(userData){
+
+      dispatch(login({userData})) }
+      else{
+        dispatch(logout())
+      }
+
+  })
+  .finally(()=>setLoading(false))
+
+},[])
+
+
+return !loading?(
+  <div className="min-h-screen flex flex-wrap
+  content-between bg-gray-400
+  "><div className='w-full block'>
+    <Header/>
+    <main>FFFFFFFF
+    {  /*<Outlet/>*/}
+
+    </main>
+    <Footer/>
+    </div>
+    </div>
+):null
+
+
+
 }
 
 export default App
